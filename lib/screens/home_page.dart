b751100b665381dae8b4551577112b7ba139e047
pankajkadyan73 <pankajkadyan73@gmail.com>
2025-04-07 19:43:54 +0000

    update

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+class AppColors {
+  static const Color blue = Colors.blue;
+}
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -10,24 +14,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final AppColors colors = AppColors();
   final FirebaseFirestore db = FirebaseFirestore.instance;
   //creat an empty list of maps which represent our tasks
   final List<Map<String, dynamic>> tasks = [];
   // Create a variable that captures the input of a text input
   final TextEditingController taskController = TextEditingController();
 
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold( 
-
-    appBar: AppBar(
-      backgroundColor: colors.blue,
-      title: Row(children: [
-
-
-      ],)
-    ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Row(
+          children: [
+            Expanded(child: Image.asset('assets/rdplogo.png', height: 80)),
+          ],
+        ),
+      ),
     );
   }
 }
