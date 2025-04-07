0657edffbec735f1ce1eb81a082ecc6986afc8cd
pankajkadyan73 <pankajkadyan73@gmail.com>
2025-04-07 21:20:50 +0000

    update

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,10 +1,10 @@
+import 'dart:nativewrappers/_internal/vm/lib/ffi_patch.dart';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-
-class AppColors {
-  static const Color blue = Colors.blue;
-}
+import 'package:table_calendar/table_calendar.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,7 +14,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final AppColors colors = AppColors();
   final FirebaseFirestore db = FirebaseFirestore.instance;
   //creat an empty list of maps which represent our tasks
   final List<Map<String, dynamic>> tasks = [];
@@ -27,11 +26,50 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Colors.blue,
         title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Expanded(child: Image.asset('assets/rdplogo.png', height: 80)),
+            Text(
+              'RDP Daily Planner',
+              style: TextStyle(
+                fontFamily: 'Caveat',
+                color: Colors.white,
+                fontSize: 32,
+              ),
+            ),
           ],
         ),
       ),
+      body: Column(
+        children: [
+          TableCalendar(
+            calendarFormat: CalendarFormat.month,
+            focusedDay: DateTime.now(),
+            firstDay: DateTime(2025),
+            lastDay: DateTime(2026),
+          ),
+        ],
+      ),
+      drawer: Drawer(),
     );
   }
+}
+
+//Build the section for adding tasks
+Widget buildAddTaskSection(nameController, addTask) {
+  return Row(
+    children: [
+      Container(
+        child: TextField(
+          maxLength: 32,
+          controller: nameController,
+          decoration: InputDecoration(
+            labelText: 'Add Task',
+            border: OutlineInputBorder(),
+          ),
+        ),
+      ),
+      ElevatedButton(onPressed: null, child: Text('Add Task')),
+    ],
+  );
 }
