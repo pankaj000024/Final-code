2c15325dd062a975d9c1728760eb9d9398abf99b
pankajkadyan73 <pankajkadyan73@gmail.com>
2025-04-08 21:07:00 +0000

    update

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,9 +1,8 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_patch.dart';
+import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:table_calendar/table_calendar.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class HomePage extends StatefulWidget {
@@ -18,7 +17,30 @@ class _HomePageState extends State<HomePage> {
   //creat an empty list of maps which represent our tasks
   final List<Map<String, dynamic>> tasks = [];
   // Create a variable that captures the input of a text input
-  final TextEditingController taskController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    fetchTasks();
+  }
+
+  //Fetch tasks from the db and also update the task list in memory
+  Future<void> fetchTasks() async {
+    final snapshots = await db.collection('tasks').orderBy('timestamp').get();
+
+    setState(() {
+      tasks.clear();
+      tasks.addAll(
+        snapshots.docs.map(
+          (doc) => {
+            'id': doc.id,
+            'name': doc.get('name'),
+            'completed': doc.get('completed') ?? false,
+          },
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,6 +70,7 @@ class _HomePageState extends State<HomePage> {
             firstDay: DateTime(2025),
             lastDay: DateTime(2026),
           ),
+          buildAddTaskSection(nameController),
         ],
       ),
       drawer: Drawer(),
@@ -56,20 +79,37 @@ class _HomePageState extends State<HomePage> {
 }
 
 //Build the section for adding tasks
-Widget buildAddTaskSection(nameController, addTask) {
-  return Row(
-    children: [
-      Container(
-        child: TextField(
-          maxLength: 32,
-          controller: nameController,
-          decoration: InputDecoration(
-            labelText: 'Add Task',
-            border: OutlineInputBorder(),
+Widget buildAddTaskSection(nameController) {
+  return Padding(
+    padding: const EdgeInsets.all(12.0),
+    child: Row(
+      children: [
+        Container(
+          decoration: BoxDecoration(color: Colors.white),
+          child: TextField(
+            maxLength: 32,
+            controller: nameController,
+            decoration: InputDecoration(
+              labelText: 'Add Task',
+              border: OutlineInputBorder(),
+            ),
           ),
         ),
-      ),
-      ElevatedButton(onPressed: null, child: Text('Add Task')),
-    ],
+        ElevatedButton(onPressed: null, child: Text('Add Task')),
+      ],
+    ),
+  );
+}
+
+Widget buildTaskList(tasks) {
+  return ListView.builder(
+    physics: NeverScrollableScrollPhysics(),
+    shrinkWrap: true,
+    itemCount: tasks.length,
+    itemBuilder: (context, index) {
+      return ListTile(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      );
+    },
   );
 }
